3c3a9e371bdbf0851ca5c6751cbfad61efac3aa3
Christian Thorjussen <christianbern@gmail.com>
2024-03-27 09:22:50 +0100

    cleaning up data and feed names Reestimating growth with clean data Saving a analysis data set

--- data_cleaning.R
+++ data_cleaning.R
@@ -2,4 +2,106 @@ rm(list = ls())
 setwd("C:/Causal-Workflow/")
 
 load(file="data/unclean_analysis_data.Rda")
+data$chicken_type <-  ifelse(data$type_of_prod == 4, "Processed", 
+                             ifelse(data$type_of_prod == 6, "Grill", 
+                                    ifelse(data$type_of_prod == 8, "Land",
+                                           ifelse(data$type_of_prod == 12, "Organic",
+                                                  ifelse(data$type_of_prod == 14, "Liveche",
+                                                         ifelse(data$type_of_prod == 17, "McDonalds",
+                                                                ifelse(data$type_of_prod == 21, "Hubbard",
+                                                                       ifelse(data$type_of_prod == 22, "Kyllinggaarden",
+                                                                              ifelse(data$type_of_prod == 29, "Unknown", NA)))))))))
+table(data$chicken_type)  
+
+grill_data <- subset(data, chicken_type == "Grill")
+grill_data <- subset(grill_data, age <= 30)
+
+grill_data$weight[grill_data$weight >= 100 & grill_data$age == 0] <- NA
+grill_data$weight[grill_data$weight <= 34 & grill_data$age == 0] <- NA
+
+for (i in 0:30){
+  grill_data$weight[grill_data$weight == 0 & grill_data$age == i]  <- NA
+  mean <- mean(grill_data$weight[grill_data$age == i], na.rm = T)
+  outlier <- 4*sd(grill_data$weight[grill_data$age == i], na.rm = T)
+  grill_data$weight[(grill_data$weight >= (mean + outlier) | grill_data$weight <= (mean - outlier) ) & grill_data$age == i]  <- NA
+}
+
+proces_data <- subset(data, chicken_type == "Processed")
+proces_data <- subset(proces_data, age <= 30) 
+proces_data$weight[proces_data$weight >= 100 & proces_data$age == 0] <- NA
+proces_data$weight[proces_data$weight <= 25 & proces_data$age == 0] <- NA
+
+for (i in 0:30){
+  proces_data$weight[proces_data$weight == 0 & proces_data$age == i]  <- NA
+  mean <- mean(proces_data$weight[proces_data$age == i], na.rm = T)
+  outlier <- 4*sd(proces_data$weight[proces_data$age == i], na.rm = T)
+  proces_data$weight[(proces_data$weight >= (mean + outlier) | proces_data$weight <= (mean - outlier) ) & proces_data$age == i]  <- NA
+}
+
+#########################################
+###Cleaning data for McDonalds chicken###
+#########################################
+mcdonald_data <- subset(data, chicken_type == "McDonalds")
+mcdonald_data <- subset(mcdonald_data, age <= 30)
+mcdonald_data$weight[mcdonald_data$weight >= 100 & mcdonald_data$age == 0] <- NA
+mcdonald_data$weight[mcdonald_data$weight <= 25 & mcdonald_data$age == 0] <- NA
+
+for (i in 0:30){
+  mcdonald_data$weight[mcdonald_data$weight == 0 & mcdonald_data$age == i]  <- NA
+  mean <- mean(mcdonald_data$weight[mcdonald_data$age == i], na.rm = T)
+  outlier <- 4*sd(mcdonald_data$weight[mcdonald_data$age == i], na.rm = T)
+  mcdonald_data$weight[(mcdonald_data$weight >= (mean + outlier) | mcdonald_data$weight <= (mean - outlier) ) & mcdonald_data$age == i]  <- NA
+}
+
+##############################################
+###Cleaning data for unknown               ###
+##############################################
+
+unknown_data <- subset(data, chicken_type == "Unknown")
+unknown_data <- subset(unknown_data, age <= 30)
+
+unknown_data$weight[unknown_data$weight >= 100 & unknown_data$age == 0] <- NA
+unknown_data$weight[unknown_data$weight <= 25 & unknown_data$age == 0] <- NA
+
+for (i in 0:30){
+  unknown_data$weight[unknown_data$weight == 0 & unknown_data$age == i]  <- NA
+  mean <- mean(unknown_data$weight[unknown_data$age == i], na.rm = T)
+  outlier <- 4*sd(unknown_data$weight[unknown_data$age == i], na.rm = T)
+  unknown_data$weight[(unknown_data$weight >= (mean + outlier) | unknown_data$weight <= (mean - outlier) ) & unknown_data$age == i]  <- NA
+}
+
+
+##############################################
+###Cleaning data for Kyllinggaarden chicken###
+##############################################
+kyllinggaarden_data <- subset(data, chicken_type == "Kyllinggaarden")
+kyllinggaarden_data <- subset(kyllinggaarden_data, age <= 30)
+
+kyllinggaarden_data$weight[kyllinggaarden_data$weight >= 100 & kyllinggaarden_data$age == 0] <- NA
+kyllinggaarden_data$weight[kyllinggaarden_data$weight <= 25 & kyllinggaarden_data$age == 0] <- NA
+
+for (i in 0:30){
+  kyllinggaarden_data$weight[kyllinggaarden_data$weight == 0 & kyllinggaarden_data$age == i]  <- NA
+  mean <- mean(kyllinggaarden_data$weight[kyllinggaarden_data$age == i], na.rm = T)
+  outlier <- 4*sd(kyllinggaarden_data$weight[kyllinggaarden_data$age == i], na.rm = T)
+  kyllinggaarden_data$weight[(kyllinggaarden_data$weight >= (mean + outlier) | kyllinggaarden_data$weight <= (mean - outlier) ) & kyllinggaarden_data$age == i]  <- NA
+}
+
+
+#Data cleaning line by line
+data <- rbind(grill_data, proces_data, mcdonald_data, kyllinggaarden_data, unknown_data)
+
+data$temp_max[data$temp_max == -99.0] <- NA
+data$temp_max[data$temp_max == 99.0] <- NA
+data$temp_max[data$temp_min == -99.0] <- NA
+data$temp_max[data$temp_min == 99.0] <- NA
+data$temp_min[data$temp_min >= 80] <- NA
+data$temp_max[data$temp_max >= 80] <- NA
+data$humidity_min[data$humidity_min > 100] <- NA
+data$humidity_max[data$humidity_max > 100] <- NA
+data$humidity_min[data$humidity_min < 1] <- NA
+data$humidity_max[data$humidity_max < 1] <- NA
+
+
+save(data, file="data/clean_data.Rda")
 
--- growth model.R
+++ growth model.R
@@ -1,8 +1,12 @@
+rm(list = ls())
 library(dplyr)
+library(tidyr)
 library(lubridate)
 library(janitor)
 library(lavaan)
 library(ggplot2)
+
+
 setwd("C:/Causal-Workflow/")
 
 load(file="data/clean_data.Rda")
@@ -17,23 +21,28 @@ long_df <- long_df %>%
   filter(sum(!is.na(weight)) >= 20)
 
 #Reshape data
-wide_df <- reshape(long_df, idvar = "id_batch", timevar = "age", direction = "wide")
-all_columns <- colnames(wide_df)
+wide_df <- pivot_wider(long_df, 
+                      names_from = age, 
+                      values_from = weight, 
+                      id_cols = id_batch,
+                      names_prefix = "weight_")
 
-# Identify columns to keep (weight.1 to weight.29)
-# This pattern matches "weight." followed by a number from 1 to 29
-columns_to_keep <- c('id_batch', grep("weight\\.(2[0-8]|1[0-9]|[1-9])$", all_columns, value = TRUE))
-wide_df <- wide_df[, columns_to_keep]
+#Keeping variables which have many observed
+wide_df <- wide_df[c("id_batch", "weight_1", "weight_2", "weight_3", "weight_4", "weight_5", "weight_6", 
+                     "weight_7", "weight_8", "weight_9", "weight_10", "weight_11", "weight_12", 
+                     "weight_13", "weight_14", "weight_15", "weight_16", "weight_17", "weight_18", 
+                     "weight_19", "weight_20", "weight_21", "weight_22", "weight_23", "weight_24", 
+                     "weight_25", "weight_26", "weight_27")]
 
 #Removing rows with missing values, i.e. we are only modeling on those with a complete set of weight observations
 wide_df <- na.omit(wide_df)
 #Specifying a growth model
-model <- ' intercept =~   1*weight.1 + 1*weight.2 + 1*weight.3 + 1*weight.4 + 1*weight.5 + 1*weight.6 + 1*weight.7 + 1*weight.8 + 1*weight.9 + 1*weight.10 + 1*weight.11 + 1*weight.12 + 1*weight.13 + 1*weight.14 + 1*weight.15 + 1*weight.16 + 1*weight.17 + 1*weight.18 + 1*weight.19 + 1*weight.20 + 1*weight.21 + 1*weight.22 + 1*weight.23 +  1*weight.24 + 1*weight.25 + 1*weight.26 + 1*weight.27 + 1*weight.28
-           growth =~   1*weight.1 + 2*weight.2 + 3*weight.3 + 4*weight.4 + 5*weight.5 + 6*weight.6 + 7*weight.7 + 8*weight.8 + 9*weight.9 + 10*weight.10 + 11*weight.11 + 12*weight.12 + 13*weight.13 + 14*weight.14 + 15*weight.15 + 16*weight.16 + 17*weight.17 + 18*weight.18 + 19*weight.19 + 20*weight.20 + 21*weight.21 + 22*weight.22 + 23*weight.23 +24*weight.24 + 25*weight.25 + 26*weight.26 + 27*weight.27 + 28*weight.28
-            sqr_growth =~   1*weight.1 + 4*weight.2 + 9*weight.3 + 16*weight.4 + 25*weight.5 + 36*weight.6 + 49*weight.7 + 64*weight.8 + 81*weight.9 + 100*weight.10 + 121*weight.11 + 144*weight.12 + 169*weight.13 + 196*weight.14 + 225*weight.15 + 256*weight.16 + 289*weight.17 + 324*weight.18 + 361*weight.19 + 400*weight.20 + 441*weight.21 + 484*weight.22 + 529*weight.23 +  576*weight.24 + 625*weight.25 + 676*weight.26 + 729*weight.27 + 784*weight.28'
+model <- ' intercept =~   1*weight_1 + 1*weight_2 + 1*weight_3 + 1*weight_4 + 1*weight_5 + 1*weight_6 + 1*weight_7 + 1*weight_8 + 1*weight_9 + 1*weight_10 + 1*weight_11 + 1*weight_12 + 1*weight_13 + 1*weight_14 + 1*weight_15 + 1*weight_16 + 1*weight_17 + 1*weight_18 + 1*weight_19 + 1*weight_20 + 1*weight_21 + 1*weight_22 + 1*weight_23 +  1*weight_24 + 1*weight_25 + 1*weight_26 + 1*weight_27
+           growth =~   1*weight_1 + 2*weight_2 + 3*weight_3 + 4*weight_4 + 5*weight_5 + 6*weight_6 + 7*weight_7 + 8*weight_8 + 9*weight_9 + 10*weight_10 + 11*weight_11 + 12*weight_12 + 13*weight_13 + 14*weight_14 + 15*weight_15 + 16*weight_16 + 17*weight_17 + 18*weight_18 + 19*weight_19 + 20*weight_20 + 21*weight_21 + 22*weight_22 + 23*weight_23 +24*weight_24 + 25*weight_25 + 26*weight_26 + 27*weight_27 
+            sqr_growth =~   1*weight_1 + 4*weight_2 + 9*weight_3 + 16*weight_4 + 25*weight_5 + 36*weight_6 + 49*weight_7 + 64*weight_8 + 81*weight_9 + 100*weight_10 + 121*weight_11 + 144*weight_12 + 169*weight_13 + 196*weight_14 + 225*weight_15 + 256*weight_16 + 289*weight_17 + 324*weight_18 + 361*weight_19 + 400*weight_20 + 441*weight_21 + 484*weight_22 + 529*weight_23 +  576*weight_24 + 625*weight_25 + 676*weight_26 + 729*weight_27' 
 
 fit <- growth(model, data=wide_df)
-summary(fit)
+# summary(fit)
 # Predict the latent variables using the fitted model and the observed data
 parameters <- predict(fit, wide_df)
 wide_df <- cbind(wide_df, parameters)
@@ -42,15 +51,14 @@ growth <- wide_df %>% select(id_batch, id, intercept, growth, sqr_growth)
 
 
 #Reshape to long data for plotting
-long <- reshape(wide_df,
-                idvar = "id",
-                timevar = "day",
-                varying = c("weight.1", "weight.2", "weight.3", "weight.4","weight.5", "weight.6", "weight.7","weight.8",
-                            "weight.9", "weight.10","weight.11", "weight.12", "weight.13", "weight.14","weight.15", "weight.16",
-                            "weight.17", "weight.18","weight.19", "weight.20", "weight.21", "weight.22","weight.23", "weight.24",
-                            "weight.25", "weight.26","weight.27", "weight.28"),
-                v.names = c("weight"),
-                direction = "long")
+long <- pivot_longer(
+  data = wide_df, 
+  cols = starts_with("weight_"), 
+  names_to = "age", 
+  values_to = "weight",
+  names_prefix = "weight_"  # Optional: remove "var" prefix from column names in 'age'
+)
+long$day <- as.numeric(long$age)
 long <- select(long, c('id', 'id_batch', 'day', 'weight'))
 
 plot_data <- merge(long, growth, by = "id")
@@ -61,7 +69,7 @@ plot_data$estimate_weight <- plot_data$intercept + plot_data$growth*plot_data$ag
 
 #plot 
 i <- sample(plot_data$id_batch.y, 1)
-obs_df <- filter(plot_data, id_batch.y == 86473)
+obs_df <- filter(plot_data, id_batch.y == i)
 obs_df <- obs_df[order(obs_df$day), ]
 rows <- nrow(obs_df)
 obs_df$x <- seq(0, rows, length.out = rows)
@@ -80,7 +88,7 @@ p1 <- ggplot(data = obs_df, aes(x = age, y = weight)) +
   xlab("Age") + 
   ylab("Weight")
 
-p1 <- p1 + geom_line(data=obs_df, aes(x,y)) + ggtitle(paste0("Batch id = ", i, ". Foredlingskylling"))
+p1 <- p1 + geom_line(data=obs_df, aes(x,y)) + ggtitle(paste0("Batch id = ",i, ". Foredlingskylling"))
 p1
 
 #Add on growth data
@@ -110,16 +118,28 @@ long_data <- subset(data, select = c('id_batch',
 # Reshape from long to wide 
 wide_data <-  distinct(long_data, id_batch, .keep_all = TRUE)
 setwd("C:/Causal-Workflow/")
-save(wide_data,file="data/analysis_wide_data.Rda")
-save(data,file="data/analysis_data.Rda")
+save(wide_data,file = "data/analysis_wide_data.Rda")
+save(data,file = "data/analysis_data.Rda")
+
+write.csv(wide_data, file = "data/analysis_wide_data.csv", row.names = FALSE)
+#MANUALLY CHANGE THE ENCODING
+library(readr)
+wide_data <- read_csv("data/analysis_wide_data.csv", locale = locale(encoding = "UTF-8"))
+
+table(wide_data$feed_name)
+
+wide_data <- wide_data %>%
+  mutate(feed_name = case_when(
+    feed_name == "Harmoni Kylling  Margin" ~ "Harmoni Kylling Margin",
+    feed_name == "Harmoni Kylling  Ressurs" ~ "Harmoni Kylling Ressurs",
+    feed_name == "Harmoni Kylling Ressurs Mc Donalds m/ Clostat" ~ "Harmoni Kylling Ressurs Mc Donalds m/Clostat",
+    feed_name == "Harmoni Kylling Vekst " ~ "Harmoni Kylling Vekst",
+    feed_name == "Harmoni kylling  Ressurs" ~ "Harmoni Kylling Ressurs",
+    feed_name == "Harmoni kylling  Spurt" ~ "Harmoni Kylling Spurt",
+    feed_name == "Harmoni kylling Spurt" ~ "Harmoni Kylling Spurt",
+    feed_name == "Harmoni kylling Vekst" ~ "Harmoni Kylling Vekst",
+    TRUE ~ feed_name  # Default case to leave other names unchanged
+  ))
 
 
-
-
-
-
-
-
-
-
-
+save(wide_data,file = "data/analysis_wide_data.Rda")
