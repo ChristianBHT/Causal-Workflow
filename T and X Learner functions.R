ac055ebe0f8fdc1251505cd63b90a1a5d4083aa8
Christian Thorjussen <christianbern@gmail.com>
2024-04-02 13:40:36 +0200

    Tested and working T-learner and X-learner functions

--- T and X Learner functions.R
+++ T and X Learner functions.R
@@ -1,12 +1,11 @@
 library(xgboost)
 library(parallel)
 library(dplyr)
-data <- data.frame(simuData(200))
-data <- data %>% select(Y, trt, X)  # The columns must be in this ordering, first outcome, then treatment indicator then the adjustment set
 # Creating a function to calculate bootstrapped statistics 
+# Function code is based on "https://rdrr.io/github/xnie/rlearner/src/R/tboost.R"
+
 T_learner <- function(data,
                       indices = NULL,
-                      objective = "reg:squarederror",
                       max_depth = c(2,3,4,5,6),
                       eta = 0.1,
                       early_stopping = 10,
@@ -29,8 +28,8 @@ T_learner <- function(data,
     data <- data[indices, ]
   }
   
-  outcome <- data[1]
-  trt <- data[2]
+  outcome <- data.frame(data[,1])
+  trt <- data[,2]
   adjustment <-  data.frame(data[, 3:ncol(data)])
   
   # Check if treatment is correctly encoded
@@ -89,7 +88,6 @@ T_learner <- function(data,
   
   optimal_nrounds <- t_0_cv$best_iteration
   
-  # Train the final model 
   t_0_final <- xgboost::xgb.train(
     params = params,
     data = data_matrix_c,  
@@ -114,12 +112,9 @@ T_learner <- function(data,
 }
 
 
-
-
 X_learner <- function(data,
                       indices = NULL,
-                      objective = "reg:squarederror",
-                      max_depth = c(2,3,4,5,6),
+                      max_depth = 6,
                       eta = 0.1,
                       early_stopping = 10,
                       nfold_t = 5,
@@ -141,8 +136,8 @@ X_learner <- function(data,
     data <- data[indices, ]
   }
   
-  outcome <- data[1]
-  trt <- data[2]
+  outcome <- data.frame(data[,1])
+  trt <- data[,2]
   adjustment <-  data.frame(data[, 3:ncol(data)])
   
   # Check if treatment is correctly encoded
@@ -176,15 +171,14 @@ X_learner <- function(data,
     early_stopping_rounds = early_stopping,
     verbose = verbose
   )
-  # Extracting the best nrounds
+
   optimal_nrounds <- t_1_cv$best_iteration
   
   
-  # Train the final model 
-  t_1_final <- xgboost::xgb.train(
+  t_1 <- xgboost::xgb.train(
     params = params,
     data = data_matrix_t,  
-    nrounds = optimal_nrounds,  # Optimal number of rounds determined from CV
+    nrounds = optimal_nrounds,  
     verbose = verbose,
     print_every_n = print_every_n
   )
@@ -201,8 +195,7 @@ X_learner <- function(data,
   
   optimal_nrounds <- t_0_cv$best_iteration
   
-  # Train the final model 
-  t_0_final <- xgboost::xgb.train(
+  t_0 <- xgboost::xgb.train(
     params = params,
     data = data_matrix_c,  
     nrounds = optimal_nrounds,
@@ -211,16 +204,100 @@ X_learner <- function(data,
   )
   
   full_data_matrix <- xgb.DMatrix(data = as.matrix(adjustment))
-  y_1_hat <- predict(t_1_final, newdata = full_data_matrix)
-  y_0_hat <- predict(t_0_final, newdata = full_data_matrix) 
-  tau_hat <- y_1_hat - y_0_hat
+  
+  mu_1_hat <- predict(t_1, newdata = full_data_matrix)
+  mu_0_hat <- predict(t_0, newdata = full_data_matrix) 
+  
+  d_t <- y_t - mu_0_hat[trt == 1] #treatment group
+  d_c <- mu_0_hat[trt == 0] - y_c #control group
+  
+  #Model imputed ITE as a function of treatment group adjustment set 
+  data_matrix_dt <- xgb.DMatrix(data = as.matrix(x_t), label = as.matrix(d_t))
+  
+  x_1_cv <- xgboost::xgb.cv(
+    data = data_matrix_dt,
+    params = params,
+    nfold = nfold_t, 
+    nrounds = nrounds, 
+    print_every_n = print_every_n,
+    early_stopping_rounds = early_stopping,
+    verbose = verbose
+  )
+  
+  optimal_nrounds <- x_1_cv$best_iteration
+  
+  x_1_fit <- xgboost::xgb.train(
+    params = params,
+    data = data_matrix_dt,  
+    nrounds = optimal_nrounds,  
+    verbose = verbose,
+    print_every_n = print_every_n
+  )
+  
+  #Model imputed ITE as a function of CONTROL group adjustment set 
+  data_matrix_dc <- xgb.DMatrix(data = as.matrix(x_c), label = as.matrix(d_c))
+  
+  x_0_cv <- xgboost::xgb.cv(
+    data = data_matrix_dc,
+    params = params,
+    nfold = nfold_t, 
+    nrounds = nrounds, 
+    print_every_n = print_every_n,
+    early_stopping_rounds = early_stopping,
+    verbose = verbose
+  )
+  
+  optimal_nrounds <- x_0_cv$best_iteration
+  
+  x_0_fit <- xgboost::xgb.train(
+    params = params,
+    data = data_matrix_dc,  
+    nrounds = optimal_nrounds,  
+    verbose = verbose,
+    print_every_n = print_every_n
+  )
+  
+  tau_1_pred = predict(x_1_fit, newdata = full_data_matrix)
+  tau_0_pred = predict(x_0_fit, newdata = full_data_matrix)
+  
+  #Calculate propensity score
+  params <- list(
+    objective = "binary:logistic",
+    eta = eta,
+    max_depth = max_depth,
+    nthread = nthread
+  )
+  
+  probs_matrix <- xgb.DMatrix(data = as.matrix(adjustment), label = as.matrix(trt))
+  
+  trt_cv <- xgboost::xgb.cv(
+    data = probs_matrix,
+    params = params,
+    nfold = nfold_t, 
+    nrounds = nrounds, 
+    print_every_n = print_every_n,
+    early_stopping_rounds = early_stopping,
+    verbose = verbose
+  )
+    
+  optimal_nrounds <- trt_cv$best_iteration
+  
+  trt_fit <- xgboost::xgb.train(
+    params = params,
+    data = probs_matrix,  
+    nrounds = optimal_nrounds,  
+    verbose = verbose,
+    print_every_n = print_every_n
+  )
+  
+  p_hat <- predict(trt_fit, newdata = probs_matrix)
+  
+  tau_hat <- tau_1_pred*(1-p_hat) + tau_0_pred*(p_hat) 
   
   ATE <- mean(tau_hat)
-  Rsquared_t <- cor(outcome, y_1_hat)^2
-  Rsquared_c <- cor(outcome, y_0_hat)^2
   
-  
-  result <- c(ATE, Rsquared_t, Rsquared_c) 
+  result <- c(ATE) 
   
   return(result)
 }
+
--- testing script (constantly overwritten).R
+++ testing script (constantly overwritten).R
@@ -3,21 +3,27 @@ simuData <- function(n) {
   X <- rnorm(n,0,1)
   p <- 1 / (1 + exp(-X))
   trt <- rbinom(n = n, size = 1, prob = p)
-  Y <- 2*trt + X + rnorm(n,0,1)
-  return(cbind(X, trt, Y))
+  Y <- 2*trt + X + rnorm(n,0,1) #Treatment effect is 2
+  return(cbind(Y, trt, X))
 }
-dat <- simuData(10)
-
+data <- simuData(3000)
+Ttest <- T_learner(data)
+Xtest <- X_learner(data)
+lm <- lm(formula = Y ~ trt + X, data = data.frame(data))
+Ttest
+Xtest
+lm$coefficients
+summary(lm)
 
 # Perform bootstrapping
 boot_result <- boot(
   data = data,
-  statistic = T_learner,
+  statistic = X_learner,
   R = 100  
 )
 
 mean(boot_result$t[,1]) 
 sd(boot_result$t[,1])
-
+hist(boot_result$t[,1], breaks = 10)
 summary(lm(formula = Y ~ trt + X, data = data))
 
