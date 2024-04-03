62c7158a42f1e697f7b1b8f1bc3589bff49a03ab
Christian Thorjussen <christianbern@gmail.com>
2024-04-03 16:12:06 +0200

    Finish total causal effect and starte refutation checks

--- Estimation total and direct effect G-comp.R
+++ Estimation total and direct effect G-comp.R
@@ -6,7 +6,7 @@ library(tidyverse)
 library(xgboost)
 library(parallel)
 library(dplyr)
-library(ggplot2)
+
 library(margins)
 
 setwd("C:/Causal-Workflow/")
@@ -66,7 +66,7 @@ Temp. -> Humidity
 Unknown -> "Feed type"
 Unknown -> Temp.
 }')
-plot(DAG)
+# plot(DAG)
 wide_data$ascites_prev <- 1000*wide_data$ascites/wide_data$chicken_last_day
 #------------------#
 #  Total Effect    #
@@ -80,7 +80,7 @@ total_data$id_slaughterhouse <- as.factor(total_data$id_slaughterhouse)
 total_data$frequent_month <- as.factor(total_data$frequent_month)
 
 
-levels = 5  # Set the number of levels other than other
+levels = 4  # Set the number of levels other than other
 total_data$feed_group = fct_lump_n(total_data$feed_name, n = levels, other_level = "Other")
 total_data$feed_group <- as.factor(total_data$feed_group)
 table(total_data$feed_group)
@@ -89,13 +89,12 @@ total_data$treatment_1 <-  ifelse(total_data$feed_group == "Toppkylling Netto", 
 total_data$treatment_2 <-  ifelse(total_data$feed_group == "Kromat Kylling 2 Laag u/k", 1, 0) #Feed 2 obs:937  
 total_data$treatment_3 <-  ifelse(total_data$feed_group == "Kromat Kylling 2 Enkel u/k", 1, 0) #Feed 3 obs: 632
 total_data$treatment_4 <-  ifelse(total_data$feed_group == "Harmoni Kylling Ressurs", 1, 0) #Feed 4: 594
-total_data$treatment_5 <-  ifelse(total_data$feed_group == "Kromat Kylling 2 Hog uten narasin", 1, 0) #Feed 5: 291
 
 
 #-----------------------------------------------------------------------------
 # Total effect treatment_1
 #-----------------------------------------------------------------------------
-data <- subset(total_data, !(treatment_2 == 1 | treatment_3 == 1 | treatment_4 == 1 | treatment_5 == 1))
+data <- subset(total_data, !(treatment_2 == 1 | treatment_3 == 1 | treatment_4 == 1 ))
 
 dummy_slaughter <- model.matrix(~ id_slaughterhouse - 1, data = data)
 dummy_month <- model.matrix(~ frequent_month - 1, data = data)
@@ -114,7 +113,7 @@ save(X_learner_boot_trt1,file="C:/Causal-Workflow/Results/X_learner_boot_trt1.Rd
 #-----------------------------------------------------------------------------
 # Total effect treatment_2
 #-----------------------------------------------------------------------------
-data <- subset(total_data, !(treatment_1 == 1 | treatment_3 == 1 | treatment_4 == 1 | treatment_5 == 1))
+data <- subset(total_data, !(treatment_1 == 1 | treatment_3 == 1 | treatment_4 == 1))
 
 dummy_slaughter <- model.matrix(~ id_slaughterhouse - 1, data = data)
 dummy_month <- model.matrix(~ frequent_month - 1, data = data)
@@ -134,7 +133,7 @@ save(X_learner_boot_trt2,file="C:/Causal-Workflow/Results/X_learner_boot_trt2.Rd
 #-----------------------------------------------------------------------------
 # Total effect treatment_3
 #-----------------------------------------------------------------------------
-data <- subset(total_data, !(treatment_1 == 1 | treatment_2 == 1 | treatment_4 == 1 | treatment_5 == 1))
+data <- subset(total_data, !(treatment_1 == 1 | treatment_2 == 1 | treatment_4 == 1))
 
 dummy_slaughter <- model.matrix(~ id_slaughterhouse - 1, data = data)
 dummy_month <- model.matrix(~ frequent_month - 1, data = data)
@@ -153,7 +152,7 @@ save(X_learner_boot_trt3,file="C:/Causal-Workflow/Results/X_learner_boot_trt3.Rd
 #-----------------------------------------------------------------------------
 # Total effect treatment_4
 #-----------------------------------------------------------------------------
-data <- subset(total_data, !(treatment_1 == 1 | treatment_2 == 1 | treatment_3 == 1 | treatment_5 == 1))
+data <- subset(total_data, !(treatment_1 == 1 | treatment_2 == 1 | treatment_3 == 1 ))
 
 dummy_slaughter <- model.matrix(~ id_slaughterhouse - 1, data = data)
 dummy_month <- model.matrix(~ frequent_month - 1, data = data)
@@ -169,158 +168,58 @@ save(T_learner_boot_trt4,file="C:/Causal-Workflow/Results/T_learner_boot_trt4.Rd
 X_learner_boot_trt4 <-  boot(data = data, statistic = X_learner, R = 250)
 save(X_learner_boot_trt4,file="C:/Causal-Workflow/Results/X_learner_boot_trt4.Rda")
 
-#-----------------------------------------------------------------------------
-# Total effect treatment_5
-#-----------------------------------------------------------------------------
-data <- subset(total_data, !(treatment_1 == 1 | treatment_2 == 1 | treatment_3 == 1 | treatment_4 == 1))
+
+#--------------------------------------------------------------------------------
+# Linear model
+#--------------------------------------------------------------------------------
+total_data$feed_group <- factor(total_data$feed_group, 
+                                levels = c("Other",
+                                           "Toppkylling Netto",
+                                           "Kromat Kylling 2 Laag u/k",
+                                           "Kromat Kylling 2 Enkel u/k", 
+                                           "Harmoni Kylling Ressurs"))
+lm_model <- lm(ascites_prev ~ feed_group +  average_out_temp + average_out_hum + average_Tmax + average_Tmin, data = total_data) # Fit the model on the resample
+summary(lm_model)
+# Calculate marginal effects for the treatment variable
+marginal_effects_treatment <- margins(lm_model, variables = "feed_group")
+save(marginal_effects_treatment,file="C:/Causal-Workflow/Results/marginal_effects_linear_model.Rda")
+
+
+#-------------------------------
+# Heterogenous effect over month
+#-------------------------------
+
+#------------------------------------
+# Dummy treatment and random outcome
+#-----------------------------------
+
+# Dummy treatment
+data <- subset(wide_data, select = c('ascites', 'frequent_month', 'average_out_hum', 'average_out_temp', 'average_Tmax', 'average_Tmin', 'id_slaughterhouse', 'ascites_prev'))
+n <- nrow(data)
+data$dummy_trt <- rbinom(n = n, size = 1, prob = 0.4)
 
 dummy_slaughter <- model.matrix(~ id_slaughterhouse - 1, data = data)
 dummy_month <- model.matrix(~ frequent_month - 1, data = data)
-data <- subset(data, select = c('ascites_prev', 'treatment_5', 'average_out_temp', 'average_out_hum', 'average_Tmax', 'average_Tmin'))
+data <- subset(data, select = c('ascites_prev', 'dummy_trt', 'average_out_temp', 'average_out_hum', 'average_Tmax', 'average_Tmin'))
 data <- cbind(data, dummy_month)
 data <- cbind(data, dummy_slaughter)
 
 data <- na.omit(data)
 org_effect <- T_learner(data, nfold_t = 10, nfold_c = 10)
 org_effect
-T_learner_boot_trt5 <-  boot(data = data, statistic = T_learner, R = 250)
-save(T_learner_boot_trt5,file="C:/Causal-Workflow/Results/T_learner_boot_trt5.Rda")
-X_learner_boot_trt5 <-  boot(data = data, statistic = X_learner, R = 250)
-save(X_learner_boot_trt5,file="C:/Causal-Workflow/Results/X_learner_boot_trt5.Rda")
+T_learner_boot_dummy <-  boot(data = data, statistic = T_learner, R = 250)
+save(T_learner_boot_dummy,file="C:/Causal-Workflow/Results/T_learner_boot_dummy.Rda")
+X_learner_boot__dummy <-  boot(data = data, statistic = X_learner, R = 250)
+save(X_learner_boot__dummy,file="C:/Causal-Workflow/Results/X_learner_boot_dummy.Rda")
 
-
-t_learner_data <- data.frame(cbind(T_learner_boot_trt1$t[,1], 
-                        T_learner_boot_trt2$t[,1], 
-                        T_learner_boot_trt3$t[,1], 
-                        T_learner_boot_trt4$t[,1], 
-                        T_learner_boot_trt5$t[,1]))
-
-df_long <- t_learner_data %>%
-  as.data.frame() %>%
-  rownames_to_column(var = "id") %>%
-  pivot_longer(cols = -id, names_to = "variable", values_to = "value")
-
-df_percentiles <- df_long %>%
-  group_by(variable) %>%
-  summarise(
-    mean = mean(value),
-    lower_perc = quantile(value, probs = 0.025),
-    upper_perc = quantile(value, probs = 0.975)
-  )
-
-ggplot() +
-  geom_point(data = df_percentiles, aes(x = variable, y = mean), color = "black", size = 3) +
-  geom_errorbar(data = df_percentiles, aes(x = variable, ymin = lower_perc, ymax = upper_perc), color = "black", width = 0.2) +
-  scale_x_discrete(labels = c('X1' = 'Toppkylling', 'X2' = 'Kromat Low', 'X3' = 'Kromat Simple', 'X4' = 'Harmoni', 'X5' = 'Kromat High')) +
-  theme_minimal() +
-  labs(title = " ",
-       x = "Feed", 
-       y = "Change in prevalence per 1000") +
-  coord_flip()
-
-ggplot(df_long, aes(x = variable, y = value)) +
-  geom_boxplot() +
-  scale_x_discrete(labels = c('X1' = 'Toppkylling', 'X2' = 'Kromat Low', 'X3' = 'Kromat Simple', 'X4' = 'Harmoni', 'X5' = 'Kromat High')) +
-  theme_minimal() +
-  labs(title = " ",
-       x = "Feed", 
-       y = "Change in prevalence per 1000") +
-  coord_flip()
-
-x_learner_data <- 
-
-#--------------------------------------------------------------------------------
-# Linear model
-#--------------------------------------------------------------------------------
-data$treatment  <- ifelse(data$treatment_2 == 1 | data$treatment_3 == 1 , NA, ifelse(data$treatment_1 == 1,1,0))
-model_1 <- lm(ascites_prev ~ treatment +  average_food + growth + sqr_growth + indoor_mean_maxtemp + frequent_month + climate_mean_temp + id_slaughterhouse + average_water + start_weight, data = data) # Fit the model on the resample
-summary(model_1)
-data$treatment  <- ifelse(data$treatment_1 == 1 | data$treatment_3 == 1 , NA, ifelse(data$treatment_2 == 1,1,0))
-model_2 <- lm(ascites_prev ~ treatment +  average_food + growth + sqr_growth + indoor_mean_maxtemp + frequent_month + climate_mean_temp + id_slaughterhouse + average_water + start_weight, data = data) # Fit the model on the resample
-summary(model_2)
-data$treatment  <- ifelse(data$treatment_1 == 1 | data$treatment_2 == 1 , NA, ifelse(data$treatment_3 == 1,1,0))
-model_3 <- lm(ascites_prev ~ treatment +  average_food + growth + sqr_growth + indoor_mean_maxtemp + frequent_month + climate_mean_temp + id_slaughterhouse + average_water + start_weight, data = data) # Fit the model on the resample
-summary(model_3)
-
+lm_dummy <- lm(ascites_prev ~ dummy_trt +  average_out_temp + average_out_hum + average_Tmax + average_Tmin, data = total_data) # Fit the model on the resample
+summary(lm_dummy)
 # Calculate marginal effects for the treatment variable
-marginal_effects_treatment_1 <- margins(model_1, variables = "treatment")
-marginal_effects_treatment_2 <- margins(model_2, variables = "treatment")
-marginal_effects_treatment_3 <- margins(model_3, variables = "treatment")
-
-extract_ame <- function(margin_effect) {
-  summary_df <- summary(margin_effect)
-  return(data.frame(AME = summary_df$AME, Lower = summary_df$lower, Upper = summary_df$upper))
-}
+dummy_effects_treatment <- margins(lm_dummy, vce = 'delta')
+save(dummy_effects_treatment,file="C:/Causal-Workflow/Results/dummy_effects_treatment.Rda")
 
 
-ame_1 <-extract_ame(marginal_effects_treatment_1)
-ame_2 <- extract_ame(marginal_effects_treatment_2)
-ame_3 <- extract_ame(marginal_effects_treatment_3)
-
-# Combine into a single data frame
-ames <- rbind(ame_1, ame_2, ame_3)
-ames$Treatment <- factor(c("Feed 1", "Feed 2", "Feed 3"))
-
-# Plot using ggplot2
-ggplot(ames, aes(x=Treatment, y=AME)) +
-  geom_point() +
-  geom_errorbar(aes(ymin=Lower, ymax=Upper), width=.2) +
-  labs(y="Average Marginal Effect", x="", title="Marginal Effects of Treatment Variables") +
-  theme_minimal()
 
 
-#------------------------------------------------
-# Creating boxplots of the distributions
-#-----------------------------------------------
-treat1 <- data.frame(T_total_boot_feed1[,1])
-treat2 <- data.frame(T_total_boot_feed2[,1])
-treat3 <- data.frame(T_total_boot_feed3[,1])
-treat1$feed <- "Feed 1"
-colnames(treat1) <- c('values', 'feed')
-treat2$feed <- "Feed 2"
-colnames(treat2) <- c('values', 'feed')
-treat3$feed <- "Feed 3"
-colnames(treat3) <- c('values', 'feed')
-df <- rbind(treat1, treat2, treat3)
 
-plot_total <- ggplot(data = df, aes(x = feed, y = values)) +
-  geom_boxplot() +
-  xlab("Growth Feed Type") +
-  ylab("Treatment effect on the prevalence (1/1000) ascites") +
-  ggtitle("T-learner: Direct causal effect") + 
-  theme(
-    plot.title = element_text(size = 16),         # Enlarge the plot title
-    axis.title.x = element_text(size = 14),       # Enlarge the x-axis label
-    axis.title.y = element_text(size = 14),       # Enlarge the y-axis label
-    axis.text.x = element_text(size = 12),        # Enlarge the x-axis text
-    axis.text.y = element_text(size = 12)         # Enlarge the y-axis text
-  )
-plot_total
 
-data$treatment_1 <-  ifelse(data$feed_group == "Toppkylling Netto", 1, 0)
-data$treatment_2 <-  ifelse(data$feed_group == "Kromat Kylling 2 Laag u/k", 1, 0)
-data$treatment_3 <-  ifelse(data$feed_group == "Harmoni Kylling Ressurs", 1, 0)
-data <- data %>% mutate(new_var1 = ifelse(feed_group == 'Toppkylling Netto', 'Feed 1', 
-                                          ifelse(feed_group == 'Kromat Kylling 2 Laag u/k', 'Feed 2', 
-                                                 ifelse(feed_group == 'Harmoni Kylling Ressurs', 'Feed 3', 
-                                                        ifelse(feed_group == 'other', 'Control group', NA)))))
-means <- data %>%
-  group_by(new_var1) %>%
-  summarise(mean_ascites_prev = mean(ascites_prev, na.rm = TRUE))
-
-# Create the boxplot and add mean values
-ggplot(data, aes(x = new_var1, y = ascites_prev)) +
-  geom_boxplot() +
-  geom_text(data = means, aes(label = round(mean_ascites_prev, 2), 
-                              y = mean_ascites_prev), 
-            position = position_dodge(width = 0.75), 
-            vjust = -0, color = "blue") +
-  ylab("Prevalence (1/1000) ascites") +
-  xlab("Growth Feed Type") +
-  theme(
-    plot.title = element_text(size = 16),         # Enlarge the plot title
-    axis.title.x = element_text(size = 14),       # Enlarge the x-axis label
-    axis.title.y = element_text(size = 14),       # Enlarge the y-axis label
-    axis.text.x = element_text(size = 12),        # Enlarge the x-axis text
-    axis.text.y = element_text(size = 12)         # Enlarge the y-axis text
-  )
---
+++ plots and tables.R
@@ -0,0 +1,147 @@
+library(ggplot2)
+load("C://Causal-Workflow/Results/T_learner_boot_trt1.Rda")
+load("C://Causal-Workflow/Results/T_learner_boot_trt2.Rda")
+load("C://Causal-Workflow/Results/T_learner_boot_trt3.Rda")
+load("C://Causal-Workflow/Results/T_learner_boot_trt4.Rda")
+
+t_learner_data <- data.frame(cbind(T_learner_boot_trt1$t[,1], 
+                                   T_learner_boot_trt2$t[,1], 
+                                   T_learner_boot_trt3$t[,1], 
+                                   T_learner_boot_trt4$t[,1]))
+
+df_long <- t_learner_data %>%
+  as.data.frame() %>%
+  rownames_to_column(var = "id") %>%
+  pivot_longer(cols = -id, names_to = "variable", values_to = "value")
+
+df_percentiles <- df_long %>%
+  group_by(variable) %>%
+  summarise(
+    mean = mean(value),
+    lower_perc = quantile(value, probs = 0.025),
+    upper_perc = quantile(value, probs = 0.975)
+  )
+
+ggplot() +
+  geom_point(data = df_percentiles, aes(x = variable, y = mean), color = "black", size = 3) +
+  geom_errorbar(data = df_percentiles, aes(x = variable, ymin = lower_perc, ymax = upper_perc), color = "black", width = 0.2) +
+  scale_x_discrete(labels = NULL) +
+  theme_bw() +
+  labs(title = " ",
+       x = "", 
+       y = "Change in prevalence per 1000") +
+  scale_y_continuous(breaks = c(-1, -0.5, 0, 0.5, 1, 1.5), limits = c(-1.25, 1.75)) +
+  theme(axis.text.x = element_text(size = 14), 
+        axis.text.y = element_text(size = 14),
+        axis.title.x = element_text(size = 16),
+        axis.title.y = element_text(size = 16)) +
+  coord_flip()
+
+
+load("C://Causal-Workflow/Results/X_learner_boot_trt1.Rda")
+load("C://Causal-Workflow/Results/X_learner_boot_trt2.Rda")
+load("C://Causal-Workflow/Results/X_learner_boot_trt3.Rda")
+load("C://Causal-Workflow/Results/X_learner_boot_trt4.Rda")
+
+x_learner_data <- data.frame(cbind(X_learner_boot_trt1$t[,1], 
+                                   X_learner_boot_trt2$t[,1], 
+                                   X_learner_boot_trt3$t[,1], 
+                                   X_learner_boot_trt4$t[,1]))
+  
+df_long <- x_learner_data %>%
+  as.data.frame() %>%
+  rownames_to_column(var = "id") %>%
+  pivot_longer(cols = -id, names_to = "variable", values_to = "value")
+
+df_percentiles <- df_long %>%
+  group_by(variable) %>%
+  summarise(
+    mean = mean(value),
+    lower_perc = quantile(value, probs = 0.025),
+    upper_perc = quantile(value, probs = 0.975)
+  )
+
+ggplot() +
+  geom_point(data = df_percentiles, aes(x = variable, y = mean), color = "black", size = 3) +
+  geom_errorbar(data = df_percentiles, aes(x = variable, ymin = lower_perc, ymax = upper_perc), color = "black", width = 0.2) +
+  theme_bw() +
+  scale_x_discrete(labels = NULL) +
+  labs(title = " ",
+       x = "", 
+       y = "Change in prevalence per 1000") +
+  scale_y_continuous(breaks = c(-1, -0.5, 0, 0.5, 1, 1.5), limits = c(-1.25, 1.75)) +
+  theme(axis.text.x = element_text(size = 14), 
+        axis.text.y = element_text(size = 14),
+        axis.title.x = element_text(size = 16),
+        axis.title.y = element_text(size = 16)) +
+  coord_flip()
+
+load("C://Causal-Workflow/Results/marginal_effects_linear_model.Rda")
+
+extract_ame <- function(margin_effect) {
+  summary_df <- summary(margin_effect)
+  return(data.frame(AME = summary_df$AME, Lower = summary_df$lower, Upper = summary_df$upper))
+}
+
+
+ame_1 <-extract_ame(marginal_effects_treatment)
+
+# Combine into a single data frame
+ame_1$Treatment <- factor(c("Harmoni", "Kromat Simple", "Kromat Low", "Toppkylling"))
+
+
+# Convert 'Treatment' to a factor with levels in the current order of the data frame
+ame_1$Treatment <- factor(ame_1$Treatment, levels = c('Toppkylling',  'Kromat Low', 'Kromat Simple', 'Harmoni'))
+levels(ame_1$Treatment)
+# Plot using ggplot2
+ggplot(ame_1, aes(x=Treatment, y=AME)) +
+  geom_point( color = "black", size = 3) +
+  geom_errorbar(aes(ymin=Lower, ymax=Upper), width=.2) +
+  theme_bw() +
+  labs(y = "Average Marginal Effect", x="", title = "Marginal Effects of Treatment Variables") +
+  labs(title = " ",
+                       x = "Feed", 
+                       y = "Change in prevalence per 1000") +
+  scale_y_continuous(breaks = c(-1, -0.5, 0, 0.5, 1, 1.5), limits = c(-1.25, 1.75)) +
+  theme(axis.text.x = element_text(size = 14), 
+        axis.text.y = element_text(size = 14),
+        axis.title.x = element_text(size = 16),
+        axis.title.y = element_text(size = 16)) +
+  coord_flip()
+
+    
+#Placebo treatment
+
+load("C:/Causal-Workflow/Results/dummy_effects_treatment.Rda")
+
+extract_ame <- function(margin_effect) {
+  summary_df <- summary(margin_effect)
+  return(data.frame(AME = summary_df$AME, Lower = summary_df$lower, Upper = summary_df$upper))
+}
+
+
+ame_1 <-extract_ame(dummy_effects_treatment)
+reg_placebo <- ame_1["dummy_trt",]
+load("C:/Causal-Workflow/Results/T_learner_boot_dummy.Rda")
+T_boot_placebo <- T_learner_boot_dummy$t[,1]
+load("C:/Causal-Workflow/Results/X_learner_boot_dummy.Rda")
+X_boot_placebo <- X_learner_boot__dummy$t[,1]
+
+
+T_placebo <- c(mean = mean(T_boot_placebo), 
+               Lower = quantile(T_boot_placebo, probs = 0.025), 
+               Upper = quantile(T_boot_placebo, probs = 0.975))
+X_placebo <- c(mean = mean(X_boot_placebo), 
+               Lower = quantile(X_boot_placebo, probs = 0.025), 
+               Upper = quantile(X_boot_placebo, probs = 0.975))
+placebo_data <- rbind(T_placebo, X_placebo, reg_placebo)
+
+
+
+
+
+
+
+
+
+
